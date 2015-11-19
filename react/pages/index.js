a73bb71caf3cbc8735e56700f8b5a0f205671507
Jørgen Bergquist <jorgen.bergquist@ntnu.no>
2015-11-19 18:38:28 +0100

    meh @ props

--- react/pages/index.js
+++ react/pages/index.js
@@ -1,11 +1,53 @@
 import React from "react";
+import translator from "../../server/lib/translator";
+import {Grid, Row, Col, Input, Button} from "react-bootstrap";
 
 var IndexPage = React.createClass({
     displayName: "IndexPage",
 
+    onRegister(e){
+        e.preventDefault();
+        console.log("Wanna register");
+        alert("FOOK");
+    },
+
     render(){
+        var __ = translator(this.props.lang);
+        console.log(this.props);
+
         return (
-            <h1>HEia mamma</h1>
+            <Grid>
+                <Row>
+                    <Col xs={12}>
+                        <section>
+                            <h1>Samklang</h1>
+                            <p>Info om awesum saus prosjekt og greier</p>
+                        </section>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12}>
+                        <form onSubmit={this.onRegister}>
+                            <Input
+                                required
+                                label={__("Name")}
+                                ref="name"
+                                type="text" />
+                            <Input
+                                required
+                                label={__("Email")}
+                                ref="email"
+                                type="email" />
+                            <Input
+                                required
+                                label={__("Password")}
+                                ref="password"
+                                type="password" />
+                            <Button type="submit" bsStyle="primary">{__("Lagre")}</Button>
+                        </form>
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 });
---
+++ server/settings.example.js
@@ -0,0 +1,12 @@
+module.exports = {
+    bunyan: {
+        name: "samklang-dev",
+        level: "debug"
+    },
+    mongo: {
+        servers: ["mongodb://localhost/samklang"],
+        replset: null
+    },
+    locales: ["en", "nb"],
+    defaultLocale: "en"
+};
