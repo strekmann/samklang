782ab1232843e2357fe11999ab033696da201723
Sigurd Gartmann <sigurdga@sigurdga.no>
2016-03-04 00:48:00 +0100

    eslint: react/prop-types

--- src/common/components/Footer.js
+++ src/common/components/Footer.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router';
 
-export default ({ id }) => (
-    <footer>
-        <hr />
-        <Grid>
-            <Row>
-                <Col xs={4}>
-                    {id ? <a href="/auth/logout">Log out</a> : '' }
-                </Col>
-                <Col xs={4}>
-                    <Link to="/sites">All sites</Link>
-                </Col>
-            </Row>
-        </Grid>
-    </footer>
-);
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <hr />
+                <Grid>
+                    <Row>
+                        <Col xs={4}>
+                            {this.props.id ? <a href="/auth/logout">Log out</a> : '' }
+                        </Col>
+                        <Col xs={4}>
+                            <Link to="/sites">All sites</Link>
+                        </Col>
+                    </Row>
+                </Grid>
+            </footer>
+        );
+    }
+}
+
+Footer.propTypes = {
+    id: React.PropTypes.string,
+};
+
+export default Footer;
